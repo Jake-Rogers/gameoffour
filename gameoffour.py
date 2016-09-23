299267ed27865531a60736a80631dc64d674b264
Chi-Cheong Weng <chicheongweng@gmail.com>
2016-09-23 10:01:24 +0800

    fixed syntax error and print_gameboard()

--- gameoffour.py
+++ gameoffour.py
@@ -3,25 +3,44 @@ import random
 
 num_of_columns = 7
 num_of_rows = 6
+check_length = 4
 
 class gameboard():
     def __init__(self):
         self.columns = []
-
+        for i in range(0, num_of_columns):
+            self.columns.append([])
+        
     # check whether column i is full
     def column_full(self, i):
         return len(self.columns[i])==num_of_rows
 
     # drop a disc on column i
     def drop_disc(self, i, d):
-        if not column_full(self, i, d):
+        if not self.column_full(i):
             self.columns[i].append(d)
 
-    # check whether there is a hit on column i
+    # convert column i to a string
+    def column2string(self, i):
+        assert i>=0 and i<num_of_columns
+        return ''.join(self.columns[i])
+
+    # convert row i to a string
+    def row2string(self, i):
+        assert i>=0 and i<num_of_rows
+        t = []
+        for col in self.columns:
+            try:
+                t.append(col[i])
+            except:
+                t.append(' ')
+        return ''.join(t)
+
+    # check whether there is a hit on a col
     def check_vertical(self, i):
         return False
 
-    # check whether there is a hit on row i
+    # check whether there is a hit on a row
     def check_horizonta(self, i):
         return False
         
@@ -29,8 +48,15 @@ class gameboard():
     def check_diagnonal(self):
         return False
 
+    def print_row(self, i):
+        assert i>=0 and i<= num_of_rows
+        s = self.row2string(i)
+        print s
+
     def print_gameboard(self):
-        pass
+        l = list(reversed(range(num_of_rows)))
+        for r in l:
+            self.print_row(r)
 
     # check if there is a hit
     def check_four(self):
@@ -53,7 +79,16 @@ def randomdraw():
 if __name__ == "__main__":
     gb = gameboard()
 
+    """
     done = False
     while not done:
         disc = randomdraw()
         print disc
+    """
+    
+    for _ in range(0, num_of_rows):
+        for c in range(0, num_of_columns):
+            disc = randomdraw()
+            print "dropping disc {0} on column {1}".format(disc, c)
+            gb.drop_disc(c, disc)
+            gb.print_gameboard()
